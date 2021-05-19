51d18c63efa304c3c14d449fa22d639cf34cb86b
Daniel McKnight <34697904+NeonDaniel@users.noreply.github.com>
2021-05-19 10:53:46 -0700

    Update license (#2)
    Update testing
    Add non-Neon core compat.
    Update imports to use neon_speech/OPM

--- neon_stt_plugin_google_cloud_streaming/__init__.py
+++ neon_stt_plugin_google_cloud_streaming/__init__.py
@@ -1,31 +1,36 @@
-#!/usr/bin/env bash
-
 # NEON AI (TM) SOFTWARE, Software Development Kit & Application Development System
+# All trademark and other rights reserved by their respective owners
+# Copyright 2008-2021 Neongecko.com Inc.
 #
-# Copyright 2008-2020 Neongecko.com Inc. | All Rights Reserved
-#
-# Notice of License - Duplicating this Notice of License near the start of any file containing
-# a derivative of this software is a condition of license for this software.
-# Friendly Licensing:
-# No charge, open source royalty free use of the Neon AI software source and object is offered for
-# educational users, noncommercial enthusiasts, Public Benefit Corporations (and LLCs) and
-# Social Purpose Corporations (and LLCs). Developers can contact developers@neon.ai
-# For commercial licensing, distribution of derivative works or redistribution please contact licenses@neon.ai
-# Distributed on an "AS IS” basis without warranties or conditions of any kind, either express or implied.
-# Trademarks of Neongecko: Neon AI(TM), Neon Assist (TM), Neon Communicator(TM), Klat(TM)
-# Authors: Guy Daniels, Daniel McKnight, Regina Bloomstine, Elon Gasper, Richard Leeds
-#
-# Specialized conversational reconveyance options from Conversation Processing Intelligence Corp.
-# US Patents 2008-2020: US7424516, US20140161250, US20140177813, US8638908, US8068604, US8553852, US10530923, US10530924
-# China Patent: CN102017585  -  Europe Patent: EU2156652  -  Patents Pending
+# Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+# following conditions are met:
+# 1. Redistributions of source code must retain the above copyright notice, this list of conditions
+#    and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions
+#    and the following disclaimer in the documentation and/or other materials provided with the distribution.
+# 3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote
+#    products derived from this software without specific prior written permission.
 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+# INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+# WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE
+# USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+from inspect import signature
 from queue import Queue
 from google.cloud import speech
 from google.cloud.speech import enums
 from google.cloud.speech import types
 from google.oauth2.service_account import Credentials
-from neon_utils.plugins.stt import StreamingSTT, StreamThread
 from neon_utils.logger import LOG
+
+try:
+    from neon_speech.stt import StreamingSTT, StreamThread
+except ImportError:
+    from ovos_plugin_manager.templates.stt import StreamingSTT, StreamThread
+
 LOG.name = "stt_plugin"
 
 
@@ -47,7 +52,12 @@ class GoogleCloudStreamingSTT(StreamingSTT):
     """
 
     def __init__(self, results_event=None, config=None):
-        super(GoogleCloudStreamingSTT, self).__init__(results_event, config)
+        if len(signature(super(GoogleCloudStreamingSTT, self).__init__).parameters) == 2:
+            super(GoogleCloudStreamingSTT, self).__init__(results_event, config)
+        else:
+            LOG.warning(f"Shorter Signature Found; config will be ignored and results_event will not be handled!")
+            super(GoogleCloudStreamingSTT, self).__init__()
+            self.results_event = None
         # override language with module specific language selection
         self.language = self.config.get('lang') or self.lang
         self.queue = None
@@ -112,4 +122,6 @@ class GoogleStreamThread(StreamThread):
                 LOG.debug(self.transcriptions)
             if self.results_event:
                 self.results_event.set()
+            if self.transcriptions:
+                self.text = self.transcriptions[0]  # Mycroft compat.
             return self.transcriptions
