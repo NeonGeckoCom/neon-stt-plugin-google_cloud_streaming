220cd11ba7b828f481fd914085ac485f5882d3a9
Daniel McKnight <34697904+NeonDaniel@users.noreply.github.com>
2022-10-12 09:24:29 -0700

    Add supported languages config with entrypoint (#32)
    * Add supported languages config with entrypoint
    Update license notices
    
    * Add idna license override
    
    Co-authored-by: Daniel McKnight <daniel@neon.ai>

--- tests/license_tests.py
+++ tests/license_tests.py
@@ -16,7 +16,8 @@ license_overrides = {
     "python-dateutil": "Apache-2.0",
     "pyparsing": "MIT",
     "typing-extensions": "PSF",
-    "exceptiongroup": "MIT"
+    "exceptiongroup": "MIT",
+    "idna": "BSD3"
 }
 # explicitly allow these packages that would fail otherwise
 whitelist = [
